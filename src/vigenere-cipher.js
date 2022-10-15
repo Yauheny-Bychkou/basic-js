9092fad9a091348ae2ae88950a13c7eb65651bde
unknown <375445433672@yandex.by>
2022-10-15 22:05:12 +0300

    feat: finally

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,15 +19,72 @@ const { NotImplementedError } = require("../extensions/index.js");
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
+const LETTERS = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 class VigenereCipheringMachine {
-  encrypt() {
-    console.log("test1");
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  constructor(f = true) {
+    this.f = f;
   }
-  decrypt() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+    return this.f == false
+      ? this.cryprt(message, key, "encrypt").reverse().join("")
+      : this.cryprt(message, key, "encrypt").join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key) throw new Error(`Incorrect arguments!`);
+    return this.f == false
+      ? this.cryprt(message, key, "decrypt").reverse().join("")
+      : this.cryprt(message, key, "decrypt").join("");
+  }
+  cryprt(message, key, action) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = [];
+    let n = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (LETTERS.indexOf(message[i]) !== -1) {
+        let count = 0;
+        if (action === "decrypt") {
+          count = (LETTERS.indexOf(message[i]) - LETTERS.indexOf(key[n]) + 26) % 26;
+        } else count = (LETTERS.indexOf(message[i]) + LETTERS.indexOf(key[n])) % 26;
+        result.push(LETTERS[count]);
+        if (n + 1 == key.length) {
+          n = 0;
+        } else n++;
+      } else {
+        result.push(message[i]);
+      }
+    }
+    return result;
   }
 }
 
